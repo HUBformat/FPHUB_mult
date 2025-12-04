d4019033a429a7893aacb23f4bf770802908ec84
JesusMLastreUMA <196177812+JesusMLastreUMA@users.noreply.github.com>
2025-12-04 10:50:06 +0100

    Fixing overflow and underflow

--- FPHUB_mult.sv
+++ FPHUB_mult.sv
@@ -54,28 +54,22 @@ special_result_for_multiplier #(E, M, special_case) special_result_inst (
 );
 
 /*
-Section: Sign Calculation
-
-The sign of the result is computed by XOR-ing the sign bits of both operands.
-This is consistent with the sign rules for multiplication of signed numbers.
-*/
-/* Variable: Z[E+M]
-   Sign bit of the result. It is the XOR of the sign bits of X and Y.
-*/
-
-
-/*
 Section: Exponent Calculation
 
 Computes the unbiased exponent of the result by adding the biased exponents of X and Y,
 then subtracting the HUB bias. The bias value is 2^(E-1), represented as 1 followed by (E-1) zeros.
 This ensures the result is properly scaled.
 */
-logic [E:0] expSum;  // Exponent result with one extra bit to handle overflow
+
 /* Variable: expSum
-   Holds the sum of the two input exponents minus the bias (2^(E-1)).
+    Holds the sum of the two input exponents minus the bias (2^(E-1)).
 */
-assign expSum = {1'b0, X[E+M-1:M]} + {1'b0, Y[E+M-1:M]} - {1'b0, 1'b1, {(E-1){1'b0}}};
+logic [E+1:0] expSum, expSum_norm;  // Exponent result with one extra bit to handle overflow
+logic [E-1:0] Z_exponent;
+
+logic exp_overflow, exp_underflow;
+
+logic Z_sign;
 
 /*
 Section: Mantissa Multiplication and Normalization
@@ -92,27 +86,52 @@ Normalization is handled based on the MSB of the result:
 If either operand is special, the `special_result` is used instead of performing multiplication.
 */
 /* Variable: multfull
-   Full-width result of multiplying two mantissas, each M bits plus 2 (implicit + ILSB).
-   Total width: 2 * (M + 2)
+    Full-width result of multiplying two mantissas, each M bits plus 2 (implicit + ILSB).
+    Total width: 2 * (M + 2)
 */
 logic [2*(M+2)-1:0] multfull;
+logic [M-1:0] Z_mantissa;
 
 always_comb begin
     multfull = {1'b1, X[M-1:0], 1'b1} * {1'b1, Y[M-1:0], 1'b1};
 
     if (X_special_case == 0 && Y_special_case == 0) begin
-        // Z sign
-        Z[E+M] = X[E+M] ^ Y[E+M];
+        
+        /*
+        Section: Sign Calculation
+
+        The sign of the result is computed by XOR-ing the sign bits of both operands.
+        This is consistent with the sign rules for multiplication of signed numbers.
+        */
+        /* Variable: Z[E+M]
+            Sign bit of the result. It is the XOR of the sign bits of X and Y.
+        */
+        Z_sign = X[E+M] ^ Y[E+M];
 
         // Select normalized mantissa bits based on overflow (MSB)
-        Z[M-1:0] = (multfull[2*(M+2)-1] == 1'b1) ? multfull[2*(M+2)-2 : M+3] : multfull[2*(M+2)-3:M+2];
+        Z_mantissa = (multfull[2*(M+2)-1] == 1'b1) ? multfull[2*(M+2)-2 : M+3] : multfull[2*(M+2)-3:M+2];
+        
+        expSum = {1'b0, 1'b0, X[E+M-1:M]} + {1'b0, 1'b0, Y[E+M-1:M]} - {1'b0, 1'b0, 1'b1, {(E-1){1'b0}}};
 
-        // Adjust exponent if overflow occurred
-        if (multfull[2*(M+2)-1] == 1'b1) begin
-            Z[E+M-1:M] = expSum[E-1:0] + 1;
-        end else begin
-            Z[E+M-1:M] = expSum[E-1:0];
+        expSum_norm = (multfull[2*(M+2)-1] == 1'b1) ? expSum + 1 : expSum;
+
+        exp_overflow = (expSum_norm[E]);
+
+        exp_underflow = (expSum_norm[E+1]);
+        
+        Z_exponent = expSum_norm[E-1:0];
+
+        if (exp_underflow) begin
+            Z_exponent = {E{1'b0}};
+            Z_mantissa = {M{1'b0}};
+        end else if (exp_overflow) begin
+            Z_exponent = {E{1'b1}};
+            Z_mantissa = {M{1'b1}};
         end
+
+        Z = {Z_sign, Z_exponent, Z_mantissa};
+
+        
     end else begin
         Z = special_result;
     end
